77e22090a4a4bc30e4fe87720103d804c0159e83
e-jj-e <101937429+e-jj-e@users.noreply.github.com>
2022-05-03 01:03:07 +0600

    feat: add solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value) {
+   // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (arguments.length > 0) {
+      this.chain.push(`( ${value} )`);
+    } else {
+      this.chain.push(`( )`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if(typeof position !== 'number' ||
+    position > this.chain.length) {
+    this.chain = [];
+    throw new Error('');
+    }
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let result = this.chain.join('~~');
+    this.chain.length = 0;
+    return result;
   }
-};
-
+}
 module.exports = {
   chainMaker
 };
